077947a2c654b756cc3249dc060377597e0a4921
Mamba1990 <h.daoudim@outlook.com>
2023-08-10 09:46:54 -0700

    01

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the sum, difference, multiple and quotient of two integers 10 and 5."""
-    from calculator_1 import add, sub, mul, div
+    """Display sum, difference, multiple and division of two integers."""
+     from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
