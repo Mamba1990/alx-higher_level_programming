c022fa9725b60c56a04bc4ab9000403b7301f2fc
Mamba1990 <h.daoudim@outlook.com>
2023-08-10 09:53:31 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-import sys
 
-i = len(sys.argv) - 1
+if __name__ == "__main__":
+    """Disply  number of and list of arguments."""
+        import sys
 
-if i == 0:
-    print("0 arguments.")
-elif i == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(i))
-for j in range(len(sys.argv) - 1):
-    print("{}: {}".format(j + 1, sys.argv[j + 1]))
+    i = len(sys.argv) - 1
+    if i == 0:
+        print("0 arguments.")
+    elif i == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(i))
+    for j in range(len(sys.argv) - 1):
+        print("{}: {}".format(j + 1, sys.argv[j + 1]))
