c9c3c67254d869ba1f026fd843b81db1a391edbc
Mamba1990 <h.daoudim@outlook.com>
2023-08-10 10:04:00 -0700

    002

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     """Display the sum of all arguments."""
     import sys
 
-    t = 0
+    add = 0
     for j in range(len(sys.argv) - 1):
-    t += int(sys.argv[j + 1])
+    add += int(sys.argv[j + 1])
     print("{}".format(t))
