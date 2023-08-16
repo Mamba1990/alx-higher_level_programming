a456ac90e6c58ba9285bd1ccb3dac03ab83b0f1d
Mamba1990 <h.daoudim@outlook.com>
2023-08-16 07:09:02 -0700

    tsk03

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,7 +3,7 @@
 
 def common_elements(set_1, set_2):
     """
-    Returns a set of common elements
-    in two sets
+    Functioin that returns a set of
+    common elements in two sets
     """
     return(set_1 & set_2)
