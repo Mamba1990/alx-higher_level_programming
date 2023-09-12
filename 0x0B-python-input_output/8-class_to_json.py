6dd5fa59d99245c5e64269ba566a9c230d4562ff
Mamba1990 <h.daoudim@outlook.com>
2023-09-12 13:10:02 -0700

    task8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Represents a Python class-to-JSON function."""
+
+
+def class_to_json(obj):
+    """Displays the dictionary represntation of a simple data structure."""
+    return obj.__dict__
