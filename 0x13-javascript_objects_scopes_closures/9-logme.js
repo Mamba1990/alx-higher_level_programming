2cdd829aeeb01d59f84987ee33d0ca8adcfb4b6c
Mamba1990 <h.daoudim@outlook.com>
2023-11-14 09:32:23 -0800

    9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let n_arg = 0;
+let arg = 0;
 
 exports.logMe = function (item) {
-  console.log(n_arg + ': ' + item);
-  n_arg++;
+  console.log(n_arg + ": " + item);
+  arg++;
 };
