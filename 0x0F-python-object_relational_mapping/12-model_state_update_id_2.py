61345c8cd30d60301bcf523bf18cf0464a3a6a17
Mamba1990 <h.daoudim@outlook.com>
2023-11-17 11:29:29 -0800

    12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" changes the name of a State object from the database 
+""" changes the name of a State object from the database
 """
 import sys
 from model_state import Base, State
