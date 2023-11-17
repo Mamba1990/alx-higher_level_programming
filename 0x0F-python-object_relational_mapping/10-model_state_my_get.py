8cb35b07a9154267c433bf171cf822cf9e8f1522
Mamba1990 <h.daoudim@outlook.com>
2023-11-17 11:06:28 -0800

    11

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Displays the State object with the name passed as argument from the database
+""" Displays the State object with the name passed as argfrom the db
 """
 import sys
 from model_state import Base, State
