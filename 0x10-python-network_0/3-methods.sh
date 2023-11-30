d141c486b65bcdeb65a69528a4383ee04c512e08
Mamba1990 <h.daoudim@outlook.com>
2023-11-30 06:37:22 -0800

    03

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displaying all HTTP methods the server of a given URL will accept.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -f2- -d" "
